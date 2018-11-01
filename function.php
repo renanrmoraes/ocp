8470abb1a246aa8b898dc7802bdb48d6d03382ba
renanrmoraes <rmoraes@f9c.com.br>
2018-11-01 10:03:35 -0300

    Update function.php

--- function.php
+++ function.php
@@ -1,5 +1,13 @@
-<?php
-for($i = 0; $i < 1000000; $i++) {
-     $a += $i;
-     echo "$$a\n";
-}
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Funcition</title>
+    </head>
+    <body>
+        <?php
+               for($i = 0; $i < 1000000; $i++) {
+                    $a += $i;
+                    echo "$$a\n";
+        ?>
+    </body>
+</html>
